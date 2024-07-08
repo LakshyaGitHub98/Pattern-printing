b27a71de7f1757d03c16723439f48ec867697622
LakshyaGitHub98 <laktripathi9839@gmail.com>
2024-07-08 19:52:27 +0530

    Adding second pattern script

--- Pattern-2.cpp
+++ Pattern-2.cpp
@@ -1,21 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
-void pattern_n()
+void pattern_2()
 {
     int n;
     cout<<"Enter the number of lines of pattern you wana to print:";
     cin>>n;
     for(int i=0;i<n;i++)
     {
-        for(int j=0;j<n;j++)
+        for(int j=0;j<(i+1);j++)
         {
-
+            cout<<"*";
         }
         cout<<endl;
     }
 }
 int main()
 {
-    pattern_n();
+    pattern_2();
     return 0;
 }
