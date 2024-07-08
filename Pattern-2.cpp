64c86fc9c2c302b85d5a1aa94badc1c97e547fd7
LakshyaGitHub98 <laktripathi9839@gmail.com>
2024-07-08 09:58:03 +0530

    Adding secong pattern script.

---
+++ Pattern-2.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+void pattern_n()
+{
+    int n;
+    cout<<"Enter the number of lines of pattern you wana to print:";
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+
+        }
+        cout<<endl;
+    }
+}
+int main()
+{
+    pattern_n();
+    return 0;
+}
