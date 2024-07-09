e1806b7e88c3c029e3ec82b3007a35cc36f52097
LakshyaGitHub98 <laktripathi9839@gmail.com>
2024-07-09 10:27:39 +0530

    Adding sventh pattern script

---
+++ Pattern-7.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+void pattern_7()
+{
+    int n;
+    cout<<"Enter the number of lines of pattern you wana to print:";
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=n;j>i;j--)//For gap printing
+        {
+            cout<<" ";
+        }
+        for(int j=1;j<=((2*i)-1);j++)//For star printing
+        {
+            cout<<"*";
+        }
+        for(int j=n;j>i;j--)//For gap printing
+        {
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
+int main()
+{
+    pattern_7();
+    return 0;
+}
