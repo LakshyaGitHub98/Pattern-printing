65697ad8c000602cac980cab83698b71ecc85c7e
LakshyaGitHub98 <laktripathi9839@gmail.com>
2024-07-09 10:59:54 +0530

    Adding ninth and tenth pattern script

---
+++ Pattern-10.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+void pattern_7()
+{
+    int n,a;
+    cout<<"Enter the number of lines of pattern you wana to print:";
+    cin>>a;
+    a++;
+    n=(a/2)+1;
+    for(int i=1;i<n;i++)
+    {
+        for(int j=1;j<=((2*i)-1);j++)//For star printing
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for(int i=n;i>0;i--)
+    {
+        for(int j=1;j<=((2*i)-1);j++)//For star printing
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
+int main()
+{
+    pattern_7();
+    return 0;
+}
---
+++ Pattern-9.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+using namespace std;
+void pattern_9()
+{
+    int n,a;
+    cout<<"Enter the number of lines of pattern you wana to print:";
+    cin>>a;
+    a++;
+    n=(a/2)+1;
+    for(int i=1;i<n;i++)
+    {
+        for(int j=n;j>i;j--)//For gap printing
+        {
+            cout<<" ";
+        }
+        for(int j=1;j<=((2*i)-1);j++)//For star printing
+        {
+            cout<<"*";
+        }
+        for(int j=n;j>i;j--)//For gap printing
+        {
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+    for(int i=n;i>0;i--)
+    {
+        for(int j=n;j>i;j--)//For gap printing
+        {
+            cout<<" ";
+        }
+        for(int j=1;j<=((2*i)-1);j++)//For star printing
+        {
+            cout<<"*";
+        }
+        for(int j=n;j>i;j--)//For gap printing
+        {
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
+int main()
+{
+    pattern_9();
+    return 0;
+}
